aac630aa39a1b43c71589df79efc1e579d13ed7e
lucas-chave <lcchavesr150@gmail.com>
2023-10-24 19:29:34 -0300

    feat: add lgi for calculator

--- App.tsx
+++ App.tsx
@@ -4,20 +4,60 @@ import Button from './src/components/Button';
 import Display from './src/components/Display';
 
 function App(): JSX.Element {
-  const [displayValue, setDisplayValue] = useState('0');
+  const [displayValueState, setDisplayValueState] = useState('0');
+  const [clearDisplayState, setClearDisplayState] = useState(false);
+  const [operation, setOperationState] = useState(null);
+  const [valuesState, setValuesState] = useState([0, 0]);
+  const [current, setCurrent] = useState(0);
 
   const addDigit = (n: any) => {
-    setDisplayValue(n);
+    if (n === '.' && displayValueState.includes('.')) {
+      return;
+    }
+
+    const clearDisplay = displayValueState === '0' || clearDisplayState;
+    const currentValue = clearDisplay ? '' : displayValueState;
+    const displayValue = currentValue + n;
+    setDisplayValueState(displayValue);
+    setClearDisplayState(false);
+
+    if (n !== '.') {
+      const newValue = parseFloat(displayValue);
+      const values = [...valuesState];
+      values[current] = newValue;
+      setValuesState(values)
+    }
   };
 
   const clearMemory = () => {
-    setDisplayValue('0');
+    setDisplayValueState('0');
   };
 
-  const setOperarion = () => {};
+  const setOperarion = (operationParam: any) => {
+    if (current === 0) {
+      setOperationState(operation);
+      setCurrent(1);
+      setClearDisplayState(true);
+    } else {
+      const equals = operationParam === '=';
+      const values = [...valuesState];
+      try {
+        values[0] = eval(`${values[0]} ${operation} ${values[1]}`);
+      } catch {
+        values[0] = valuesState[0] 
+      }
+
+      values[1] = 0;
+      setDisplayValueState(String(values[0]));
+      setOperationState(equals ? null : operation);
+      setCurrent(equals ? 0 : 1);
+      setClearDisplayState(!equals);
+      setValuesState(values);
+    }
+  };
   return (
     <View style={styles.container}>
-      <Display value={displayValue} />
+      <Display value={displayValueState} />
       <View style={styles.buttons}>
         <Button triple onClick={clearMemory} label={'AC'} />
         <Button operation label={'/'} onClick={setOperarion} />
@@ -34,7 +74,7 @@ function App(): JSX.Element {
         <Button label={'3'} onClick={addDigit} />
         <Button label={'+'} operation onClick={setOperarion} />
         <Button label={'0'} double onClick={addDigit} />
-        <Button label={'.'} onClick={setOperarion} />
+        <Button label={'.'} onClick={addDigit} />
         <Button label={'='} operation onClick={setOperarion} />
       </View>
     </View>
